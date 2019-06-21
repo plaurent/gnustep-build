d73839e48ecb3ce10f979253a4ea5d613ddf764d
Patryk Laurent <patryk@pakl.net>
2019-06-20 22:33:01 -0700

    adding initial arm ubuntu build

--- debian-9-clang-8.0-runtime-2.0/GNUstep-buildon-debian9.sh
+++ debian-9-clang-8.0-runtime-2.0/GNUstep-buildon-debian9.sh
@@ -194,7 +194,7 @@ rm -Rf build
 mkdir build && cd build
 cmake .. -DCMAKE_C_COMPILER=${CC} \
 -DCMAKE_CXX_COMPILER=${CXX} \
--DCMAKE_BUILD_TYPE=Release
+-DCMAKE_BUILD_TYPE=Release \
 -DUSE_GOLD_LINKER=YES
 make
 sudo -E make install
