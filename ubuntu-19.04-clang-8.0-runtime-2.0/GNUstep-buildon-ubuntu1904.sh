bfa09ce2518fe33d02beeec98fbb487b1eb633cb
Patryk Laurent <patryk@pakl.net>
2019-11-29 09:04:51 -0800

    make clang-8 explicit in ub1904 build script

--- ubuntu-19.04-clang-8.0-runtime-2.0/GNUstep-buildon-ubuntu1904.sh
+++ ubuntu-19.04-clang-8.0-runtime-2.0/GNUstep-buildon-ubuntu1904.sh
@@ -25,7 +25,7 @@ sudo apt update
 echo -e "\n\n${GREEN}Installing dependencies...${NC}"
 
 sudo apt-get update
-sudo apt -y install clang git cmake libffi-dev libxml2-dev \
+sudo apt -y install clang-8 clang++-8 build-essential wget git cmake libffi-dev libxml2-dev \
 libgnutls28-dev libicu-dev libblocksruntime-dev libkqueue-dev libpthread-workqueue-dev autoconf libtool \
 libjpeg-dev libtiff-dev libffi-dev libcairo-dev libx11-dev libxt-dev libxft-dev
 
