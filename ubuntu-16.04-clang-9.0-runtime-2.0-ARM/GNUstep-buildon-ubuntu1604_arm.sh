efd91ba5ea1d6bad972bcb1f461ecf3e8f3c39b5
Patryk Laurent <plaurent@me.com>
2019-12-26 01:43:35 +0000

    move ARM to clang9 / runtime 2.0

--- ubuntu-16.04-clang-9.0-runtime-2.0-ARM/GNUstep-buildon-ubuntu1604_arm.sh
+++ ubuntu-16.04-clang-9.0-runtime-2.0-ARM/GNUstep-buildon-ubuntu1604_arm.sh
@@ -14,7 +14,7 @@ GREEN=`tput setaf 2`
 NC=`tput sgr0` # No Color
 
 # Set to true to also build and install apps
-APPS=false
+APPS=true
 
 # Set to true to pause after each build to verify successful build and installation
 PROMPT=false
@@ -24,19 +24,35 @@ sudo apt update
 
 echo -e "\n\n${GREEN}Installing dependencies...${NC}"
 
-sudo dpkg --add-architecture i386  # Enable 32-bit repos for libx11-dev:i386
-sudo apt-get update
 sudo apt -y install clang git libffi-dev libxml2-dev \
-libgnutls28-dev libicu-dev libblocksruntime-dev libkqueue-dev libpthread-workqueue-dev autoconf libtool \
-libjpeg-dev libtiff-dev libffi-dev libcairo-dev libx11-dev:i386 libxt-dev libxft-dev
+libgnutls28-dev libicu-dev libblocksruntime-dev  libpthread-workqueue-dev autoconf libtool \
+libjpeg-dev libtiff-dev libffi-dev libcairo-dev libx11-dev libxt-dev libxft-dev
+
+echo “Getting updated libstdc++6 for GLIBCXX for clang9”
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install gcc-4.9
+sudo apt-get upgrade libstdc++6
+
+echo “Getting clang9 binaries for Aarch64”
+wget http://releases.llvm.org/9.0.0/clang+llvm-9.0.0-aarch64-linux-gnu.tar.xz
+tar xvfz clang+llvm-9.0.0-aarch64-linux-gnu.tar.xz
+ln -s clang+llvm-9.0.0-aarch64-linux-gnu/bin/clang-9 clang+llvm-9.0.0-aarch64-linux-gnu/bin/clang++-9
+
+export PATH=`pwd`/clang+llvm-9.0.0-aarch64-linux-gnu/bin/:$PATH
+echo "export PATH=`pwd`/clang+llvm-9.0.0-aarch64-linux-gnu/bin/:\$PATH" >> ~/.bashrc
+
+# Set clang as compiler
+export CC=clang-9
+export CXX=clang++-9
 
 wget https://github.com/Kitware/CMake/releases/download/v3.15.5/cmake-3.15.5.tar.gz
 tar xfz cmake-3.15.5.tar.gz
 cd cmake-3.15.5
 ./bootstrap -- -DCMAKE_BUILD_TYPE:STRING=Release
-make
+make -j4
 sudo make install
-
+cd ..
 
 if [ "$APPS" = true ] ; then
   sudo apt -y install curl
@@ -46,10 +62,7 @@ fi
 mkdir GNUstep-build
 cd GNUstep-build
 
-# Set clang as compiler
-export CC=clang
-export CXX=clang++
-export RUNTIME_VERSION=gnustep-1.9
+export RUNTIME_VERSION=gnustep-2.0
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
 export LD=/usr/bin/ld.gold
 export LDFLAGS="-fuse-ld=gold -L/usr/local/lib"
@@ -57,13 +70,14 @@ export LDFLAGS="-fuse-ld=gold -L/usr/local/lib"
 
 # Checkout sources
 echo -e "\n\n${GREEN}Checking out sources...${NC}"
-#git clone https://github.com/nickhutchinson/libdispatch.git
 git clone https://github.com/apple/swift-corelibs-libdispatch
-git clone https://github.com/plaurent/libdispatch.git
 git clone https://github.com/gnustep/libobjc2.git
 cd libobjc2
-  git checkout 1.9  # 2.0 and onward require clang8 or newer
+ git submodule init
+ git submodule sync
+ git submodule update
 cd ..
+
 git clone https://github.com/gnustep/tools-make.git
 git clone https://github.com/gnustep/libs-base.git
 git clone https://github.com/gnustep/libs-gui.git
@@ -77,7 +91,7 @@ if [ "$APPS" = true ] ; then
 fi
 
 showPrompt
-
+set -e
 # Build GNUstep make first time
 echo -e "\n\n"
 echo -e "${GREEN}Building GNUstep-make for the first time...${NC}"
@@ -89,7 +103,7 @@ sudo -E make install
 
 . /usr/GNUstep/System/Library/Makefiles/GNUstep.sh
 echo ". /usr/GNUstep/System/Library/Makefiles/GNUstep.sh" >> ~/.bashrc
-echo "export RUNTIME_VERSION=gnustep-1.9" >> ~/.bashrc
+echo "export RUNTIME_VERSION=gnustep-2.0" >> ~/.bashrc
 
 showPrompt
 
@@ -100,12 +114,13 @@ cd ../swift-corelibs-libdispatch
 rm -Rf build
 mkdir build && cd build
 cmake .. -DCMAKE_C_COMPILER=${CC} \
--DCMAKE_CXX_COMPILER=${CXX} \
--DCMAKE_BUILD_TYPE=Release \
--DUSE_GOLD_LINKER=YES
-make
+	-DCMAKE_CXX_COMPILER=${CXX} \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DUSE_GOLD_LINKER=YES
+make -j4
 sudo -E make install
 sudo ldconfig
+
 
 ## Build libdispatch
 #echo -e "\n\n"
